f09e9f96618bb841b72150423db74e6bb0e1dc14
pablocoba <contact.p4ul@gmail.com>
2024-02-12 14:17:19 +0100

    12/02/2024
    Update programa

--- Eclipse/Notas/src/music/Main.java
+++ Eclipse/Notas/src/music/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		int num = 20;
 		Scanner teclado = new Scanner(System.in);
 		Nota nota = new Nota();
@@ -13,13 +13,13 @@ public class Main {
 	}
 
 	private static void menu(Scanner teclado, Nota nota, int num) {
-		int opc = 2;
+		int opc = 1;
 		
 		
 		
-		while(opc != 5) {
+		while(opc != 9) {
 			switch (opc) {
-			case 2:
+			case 1:
 
 				opc = bucleMain(teclado,nota,num);
 				break;
@@ -43,13 +43,13 @@ public class Main {
 			num = teclado.nextInt();
 			switch(num) {
 			case 1:
-				nota.generarNota();
-				System.out.println(nota);
-				break;
-			case 2:
-				System.out.println("Se silenciará el menú, para reactivarlo, pulsa 2.");
+				System.out.println("Se silenciará el menú, para reactivarlo, pulsa 1.");
 				num = 20;
 				break;
+			case 2:
+				nota.generarNota();
+				System.out.println(nota);
+				break;	
 			case 3:
 				nota.generarNota();
 				
@@ -62,15 +62,28 @@ public class Main {
 				System.out.println(nota);
 				break;
 			case 5:
+				nota.quinta();
+				System.out.println(nota.getQuinta()+"<-quinta");
+				String quinta = teclado.next();
+				if(nota.getQuinta().equals(quinta)) {
+					System.out.println("Correcto. La quinta de " + nota.getNota() + " es "+nota.getQuinta());
+				}
+				else System.out.println("No es correcto. La quinta de "+nota.getNota()+" es "+nota.getQuinta());
+				break;
+			case 6:
+				casoIntervalo(teclado, nota);
+			case 9:
 				break;
 			default:
 				break;
 			}
 			
-		}while(num != 5 && num != 20);
+		}while(num != 9 && num != 20);
 		
 		return num;
 	}
+
+
 	private static int bucleMainSilenced(Scanner teclado, Nota nota, int num) {
 
 		do {
@@ -78,12 +91,12 @@ public class Main {
 			num = teclado.nextInt();
 			switch(num) {
 			case 1:
-				nota.generarNota();
-				System.out.println(nota);
+				System.out.println("Se habilitará el menú, para volver a desactivarlo, pulsa 1.");
+				num = 1;
 				break;
 			case 2:
-				System.out.println("Se habilitará el menú, para volver a desactivarlo, pulsa 2.");
-				num = 2;
+				nota.generarNota();
+				System.out.println(nota);
 				break;
 			case 3:
 				nota.generarNota();
@@ -97,27 +110,106 @@ public class Main {
 				System.out.println(nota);
 				break;
 			case 5:
+				nota.quinta();
+				System.out.println(nota.getQuinta()+"<-quinta");
+				String quinta = teclado.next();
+				if(nota.getQuinta().equals(quinta)) {
+					System.out.println("Correcto. La quinta de " + nota.getNota() + " es "+nota.getQuinta());
+				}
+				else System.out.println("No es correcto. La quinta de "+nota.getNota()+" es "+nota.getQuinta());
+				break;
+			case 6:
+				casoIntervalo(teclado, nota);
+			case 9:
 				break;
 			default:
 				break;
 				
 			}
 			
-		}while(num != 5 && num != 2);
+		}while(num != 9 && num != 1);
 		return num;
 	}
 
 	private static void textMenu() {
 		System.out.println("Selecciona una opción: ");
-		System.out.println("1. Nueva nota ");
-		System.out.println("2. Silenciar menú");
+		System.out.println("1. Silenciar menú");
+		System.out.println("2. Nueva nota ");
 		System.out.println("3. Nueva nota + cuerda");
 		System.out.println("4. Nueva nota (posibilidad de b y #)");
-		System.out.println("5. Salir");
+		System.out.println("5. Hallar la quinta");
+		System.out.println("6. Hallar Intervalo");
+		System.out.println("9. Salir");
 	}
 
-	private static void hideMenu() {
-		
+	private static String intAstringIntervalo(Nota nota) {
+		String notaIntervalo = null;
+		switch(nota.getInterval()) {
+		case 2:
+			notaIntervalo = "segunda";
+			break;
+		case 3:
+			notaIntervalo = "tercera";
+			break;
+		case 4:
+			notaIntervalo = "cuarta";
+			break;
+		case 5:
+			notaIntervalo = "quinta";
+			break;
+		case 6:
+			notaIntervalo = "sexta";
+			break;
+		case 7:
+			notaIntervalo = "septima";
+			break;
+		}
+		return notaIntervalo;
+	}
+
+	private static void casoIntervalo(Scanner teclado, Nota nota) {
+		nota.intervalo();
+		String respuesta = teclado.next();
+		String notaIntervalo = intAstringIntervalo(nota);
+		switch(nota.getInterval()) {
+		case 2:
+			
+			if(Character.toString(nota.getSegunda().charAt(nota.getSegunda().length()-1)).equals(respuesta)) {
+				System.out.println("Correcto. La " + notaIntervalo + " de " + nota.getNota() + " es "+nota.getSegunda());
+			}
+			else System.out.println("No es correcto. La "+ notaIntervalo +" de "+nota.getNota()+" es "+Character.toString(nota.getSegunda().charAt(nota.getSegunda().length()-1)));
+			break;
+		case 3:
+			if(nota.getTercera().equals(respuesta)) {
+				System.out.println("Correcto. La " + notaIntervalo + " de " + nota.getNota() + " es "+nota.getTercera());
+			}
+			else System.out.println("No es correcto. La "+ notaIntervalo +" de "+nota.getNota()+" es "+nota.getTercera());
+			break;
+		case 4:
+			if(nota.getCuarta().equals(respuesta)) {
+				System.out.println("Correcto. La " + notaIntervalo + " de " + nota.getNota() + " es "+nota.getCuarta());
+			}
+			else System.out.println("No es correcto. La "+ notaIntervalo +" de "+nota.getNota()+" es "+nota.getCuarta());
+			break;
+		case 5:
+			if(nota.getQuinta().equals(respuesta)) {
+				System.out.println("Correcto. La " + notaIntervalo + " de " + nota.getNota() + " es "+nota.getQuinta());
+			}
+			else System.out.println("No es correcto. La "+ notaIntervalo +" de "+nota.getNota()+" es "+nota.getQuinta());
+			break;
+		case 6:
+			if(nota.getSexta().equals(respuesta)) {
+				System.out.println("Correcto. La " + notaIntervalo + " de " + nota.getNota() + " es "+nota.getSexta());
+			}
+			else System.out.println("No es correcto. La "+ notaIntervalo +" de "+nota.getNota()+" es "+nota.getSexta());
+			break;
+		case 7:
+			if(nota.getSeptima().equals(respuesta)) {
+				System.out.println("Correcto. La " + notaIntervalo + " de " + nota.getNota() + " es "+nota.getSeptima());
+			}
+			else System.out.println("No es correcto. La "+ notaIntervalo +" de "+nota.getNota()+" es "+nota.getSeptima());
+			break;
+		}
 	}
 
 }
--- Eclipse/Notas/src/music/Nota.java
+++ Eclipse/Notas/src/music/Nota.java
@@ -2,11 +2,203 @@ package music;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 public class Nota {
 	
 	private String nota;
 	private String cuerda;
+	//TODO estoy intentando hacer que funcione el programa 6 insertando "Do" o "do" o "C" o "c"
+	private String[] notas = new String[] {
+			/*0*/"Do","C",
+			/*2*/"Re","D",
+			/*4*/"Mi","E",
+			/*6*/"Fa","F",
+			/*8*/"Sol","G",
+			/*10*/"La","A",
+			/*12*/"Si","B"
+	};
+	private String segunda;
+	private String tercera;
+	private String cuarta;
+	private String quinta;
+	private String sexta;
+	private String septima;
 	
+	private String tonalidad;
+	private int interval;
+	
+	public void quinta() {
+		generarNota();
+		String notaRaw = this.nota;
+		char nota = notaRaw.charAt(notaRaw.length()-1);
+		this.nota = Character.toString(nota);
+		System.out.println("Cuál es la quinta de "+ this.nota + "?");
+		String quinta = null;
+		
+		switch(nota) {
+		case 'C':
+			quinta = "G";
+			break;
+		case 'D':
+			quinta = "A";
+			break;
+		case 'E':
+			quinta = "B";
+			break;
+		case 'F':
+			quinta = "C";
+			break;
+		case 'G':
+			quinta = "D";
+			break;
+		case 'A':
+			quinta = "E";
+			break;
+		case 'B':
+			quinta = "F";
+			break;
+		default:
+			System.out.println("Error.");
+			break;
+		}
+
+		this.quinta = quinta;
+	}
+	public void tercera() {
+		generarNota();
+		String notaRaw = this.nota;
+		char nota = notaRaw.charAt(notaRaw.length()-1);
+		this.nota = Character.toString(nota);
+		System.out.println("Cuál es la tercera de "+ this.nota + "?");
+		String tercera = null;
+		
+		switch(nota) {
+		case 'C':
+			tercera = "E";
+			break;
+		case 'D':
+			tercera = "F";
+			break;
+		case 'E':
+			tercera = "G";
+			break;
+		case 'F':
+			tercera = "A";
+			break;
+		case 'G':
+			tercera = "B";
+			break;
+		case 'A':
+			tercera = "C";
+			break;
+		case 'B':
+			tercera = "D";
+			break;
+		default:
+			System.out.println("Error.");
+			break;
+		}
+
+		this.tercera = tercera;
+	}
+	public void intervalo() {
+		Random rndNum = new Random();
+		int interval = rndNum.nextInt(2,8);
+		this.interval = interval;
+		generarNota();
+		String notaRaw = this.nota;
+		char nota = notaRaw.charAt(notaRaw.length()-1);
+		this.nota = Character.toString(nota);
+		String newNota;
+		for (int i = 1; i <= notas.length; i=i+2) {
+			if(Character.toString(nota).equals(notas[i])) {
+				newNota = notas[i-1];
+			}
+		}
+		switch(interval) {
+		case 2:
+			System.out.println("Cuál es la segunda de "+ this.nota + "?");
+			break;
+		case 3:
+			System.out.println("Cuál es la tercera de "+ this.nota + "?");
+			break;
+		case 4:
+			System.out.println("Cuál es la cuarta de "+ this.nota + "?");
+			break;
+		case 5:
+			System.out.println("Cuál es la quinta de "+ this.nota + "?");
+			break;
+		case 6:
+			System.out.println("Cuál es la sexta de "+ this.nota + "?");
+			break;
+		case 7:
+			System.out.println("Cuál es la septima de "+ this.nota + "?");
+			break;
+		}
+		
+		switch(nota) {
+		case 'C':
+			this.segunda = "D";
+			this.tercera = "E";
+			this.cuarta = "F";
+			this.quinta = "G";
+			this.sexta = "A";
+			this.septima = "B";
+			break;
+		case 'D':
+			this.segunda = "E";
+			this.tercera = "F";
+			this.cuarta = "G";
+			this.quinta = "A";
+			this.sexta = "B";
+			this.septima = "C";
+			break;
+		case 'E':
+			this.segunda = "F";
+			this.tercera = "G";
+			this.cuarta = "A";
+			this.quinta = "B";
+			this.sexta = "C";
+			this.septima = "D";
+			break;
+		case 'F':
+			this.segunda = "G";
+			this.tercera = "A";
+			this.cuarta = "B";
+			this.quinta = "C";
+			this.sexta = "D";
+			this.septima = "E";
+			break;
+		case 'G':
+			this.segunda = "A";
+			this.tercera = "B";
+			this.cuarta = "C";
+			this.quinta = "D";
+			this.sexta = "E";
+			this.septima = "F";
+			break;
+		case 'A':
+			this.segunda = "B";
+			this.tercera = "C";
+			this.cuarta = "D";
+			this.quinta = "E";
+			this.sexta = "F";
+			this.septima = "G";
+			break;
+		case 'B':
+			this.segunda = "C";
+			this.tercera = "D";
+			this.cuarta = "E";
+			this.quinta = "F";
+			this.sexta = "G";
+			this.septima = "A";
+			break;
+		default:
+			System.out.println("Error.");
+			break;
+		}
+
+	}
 	public Nota() {
 		this.nota=generarNota();
 	}
@@ -16,6 +208,39 @@ public class Nota {
 	private int lastNum2 = 10;
 	private int lastNum3 = 10;
 	private int lastNum4 = 10;
+	
+	public void generarTonalidad() {
+		Random rndNum = new Random();
+		int rnd = rndNum.nextInt(1,8);
+		switch(rnd) {
+		case 1:
+			this.tonalidad = "C";
+			break;
+		case 2:
+			this.tonalidad = "D";
+			break;
+		case 3:
+			this.tonalidad = "E";
+			break;
+		case 4:
+			this.tonalidad = "F";
+			break;
+		case 5:
+			this.tonalidad = "G";
+			break;
+		case 6:
+			this.tonalidad = "A";
+			break;
+		case 7:
+			this.tonalidad = "B";
+			break;
+		default:
+			this.tonalidad = "C";
+			break;
+		}
+	}
+	
+	
 	
 	public String generarCuerda() {
 		
@@ -71,69 +296,69 @@ public class Nota {
 		switch(num) {
 		case 1:
 			if(bsos == '#') {
-				note = "\nDo#/C#\n";
+				note = "Do#/C#";
 			}
-				else note = "\nDo/C\n";
+				else note = "Do/C";
 			break;
 		case 2:
 			if(bsos == '#') {
-				note = "\nRe#/D#\n";
+				note = "Re#/D#";
 			}
 			else if(bsos == 'b') {
-				note = "\nReb/Db\n";
+				note = "Reb/Db";
 			}
-			else note = "\nRe/D\n";
+			else note = "Re/D";
 			break;
 		case 3:
 			if(bsos == 'b') {
-				note = "\nMib/Eb\n";
+				note = "Mib/Eb";
 			}
-			else note = "\nMi/E\n";
+			else note = "Mi/E";
 			break;
 		case 4:
 			if(bsos == '#') {
-				note = "\nFa#/F#\n";
+				note = "Fa#/F#";
 			}
-			else note = "\nFa/F\n";
+			else note = "Fa/F";
 			break;
 		case 5:
 			if(bsos == '#') {
-				note = "\nSol#/G#\n";
+				note = "Sol#/G#";
 			}
 			else if(bsos == 'b') {
-				note = "\nSolb/Gb\n";
+				note = "Solb/Gb";
 			}
-			else note = "\nSol/G\n";
+			else note = "Sol/G";
 
 			break;
 		case 6:
 			if(bsos == '#') {
-				note = "\nLa#/A#\n";
+				note = "La#/A#";
 			}
 			else if(bsos == 'b') {
-				note = "\nLab/Ab\n";
+				note = "Lab/Ab";
 			}
-			else note = "\nLa/A\n";
+			else note = "La/A";
 			
 			break;
 		case 7:
 			if(bsos == 'b') {
-				note = "\nSib/Bb\n";
+				note = "Sib/Bb";
 			}
-			else note = "\nSi/B\n";
+			else note = "Si/B";
 			break;
 		default:
-			note = "\nDo/C\n";
+			note = "Do/C";
 			break;
 		}
 		lastNum2 = lastNum;
 		lastNum = num;
 		this.nota = note;
-		return note;
+		return "\n"+note +"\n";
 	}
 	
 	public String generarNota() {
-		String note = "Do/C\n";
+		String note = "Do/C";
 		Random rnd = new Random();
 		int num = 0;
 
@@ -143,40 +368,63 @@ public class Nota {
 
 		switch(num) {
 		case 1:
-			note = "Do/C\n";
+			note = "Do/C";
 			break;
 		case 2:
-			note = "Re/D\n";
+			note = "Re/D";
 			break;
 		case 3:
-			note = "Mi/E\n";
+			note = "Mi/E";
 			break;
 		case 4:
-			note = "Fa/F\n";
+			note = "Fa/F";
 			break;
 		case 5:
-			note = "Sol/G\n";
+			note = "Sol/G";
 			break;
 		case 6:
-			note = "La/A\n";
+			note = "La/A";
 			break;
 		case 7:
-			note = "Si/B\n";
+			note = "Si/B";
 			break;
 		default:
-			note = "Do/C\n";
+			note = "Do/C";
 			break;
 		}
 		lastNum2 = lastNum;
 		lastNum = num;
 		this.nota = note;
-		return note;
+		return note + "\n";
 	}
 	
 	public String getCuerda() {
 		return this.cuerda;
 	}
-	
+	public String getSegunda() {
+		return this.segunda;
+	}
+	public String getTercera() {
+		return this.tercera;
+	}
+	public String getCuarta() {
+		return this.cuarta;
+	}
+	public String getQuinta() {
+		return this.quinta;
+	}
+	public String getSexta() {
+		return this.sexta;
+	}
+	public String getSeptima() {
+		return this.septima;
+	}
+	public String getNota() {
+		return this.nota;
+	}
+	public int getInterval() {
+		return this.interval;
+	}
 	public char generarBemol(int rnd) {
 		if(rnd == 0 ||rnd == 1 || rnd == 2) {
 			return 'b';
